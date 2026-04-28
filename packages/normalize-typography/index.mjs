d09ddbff3b5f48aeb4267c1b35f9e8e8b40dcd8e
Artem Grodetskii <gal20040@gmail.com>
2026-04-28 19:27:50 +0400

    fix: defined console.

--- packages/normalize-typography/index.mjs
+++ packages/normalize-typography/index.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { glob } from 'glob';
+import console from 'node:console';
 import fs from 'node:fs';
 import path from 'node:path';
 import process from 'node:process';
